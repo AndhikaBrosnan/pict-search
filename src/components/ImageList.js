30b9700496d302834d32abadf511fe27afaa91b1
Andhika Brosnan <andhikabrosnan@gmail.com>
2021-07-01 20:24:05 +0700

    Final Pics with grid

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
 import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
-  const images = props.images.map(({ id, description, urls }) => {
+  const images = props.images.map((image) => {
     //nama objectnya image. Tapi, clean code dengan memanggil child-nya saja
-    return <img alt={description} key={id} src={urls.regular} />;
+    return <ImageCard key={image.id} image={image} />;
   });
   return <div className="image-list">{images}</div>;
 };
